89c319f104689c56ae488688b3d1458762b08066
Carlos K <carloskotachoamd@hotmail.com>
2019-08-02 15:45:21 -0300

    Refatorando

--- assets/resources/scripts/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 300){
-            $('nav').addClass('red');
-        }else{
-            $('nav').removeClass('red');
-        }
-    });
-});
